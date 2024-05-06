f74851846f01bb9f09f5c08f2021ea70b09287fc
natidever <natnael7820@gmail.com>
2024-05-06 15:02:47 +0300

    bottom navigations

---
+++ lib/pages/mainscreens/main_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/pages/authentication/login.dart';
+import 'package:frontend/pages/landing_page.dart';
+import 'package:frontend/pages/mainscreens/homepage.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<Widget> pages = [const HomePage(), LandingPage(), Login()];
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (value) => {
+          setState(() {
+            currentPage = value;
+          })
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: "Info"),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: "Post"),
+        ],
+      ),
+    );
+  }
+}
