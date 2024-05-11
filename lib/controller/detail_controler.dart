a75df297be59df37ababe05be92f2aba4404e208
yihenew <yihenewu89@gmail.com>
2024-05-12 00:34:08 +0300

    the slide is finished

--- lib/controller/detail_controler.dart
+++ lib/controller/detail_controler.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class DetailControler extends GetxController{
+  final PageController pageController = PageController(initialPage: 0);
 }
--- lib/pages/mainscreens/detail.dart
+++ lib/pages/mainscreens/detail.dart
@@ -1,9 +1,41 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:frontend/constants.dart';
+import 'package:frontend/controller/detail_controler.dart';
 import 'package:get/get.dart';
-class DetailPage extends StatelessWidget {
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+class DetailPage extends StatefulWidget {
 DetailPage({super.key});
-  final PageController pageController = PageController();
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  final Controler = Get.find<DetailControler>();
+  Timer? _timer;
+
+  void startTimer(){
+    _timer = Timer.periodic(Duration(seconds: 4), (timer) { 
+      if (Controler.pageController.page == 2){
+          Controler.pageController.animateToPage(0,duration: Duration(milliseconds: 100), curve: Curves.easeInOut);
+      }else{
+        Controler.pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,23 +53,42 @@ DetailPage({super.key});
                 ),
             ),
             backgroundColor: primaryColor,
-            expandedHeight: 300.0,
+            expandedHeight: 250.0,
             flexibleSpace: FlexibleSpaceBar(
-              background:PageView(
-                children: [
-                  Container(
-                    color: Colors.red,
-                  ),
-                  Container(
-                    color: Colors.green,
-                  ),
-                  Container(
-                    color: Colors.yellow,
-                  ),
-                ],
+              background:Stack(
+                children:[ 
+                  PageView(
+                  controller:Controler.pageController,
+                  children: [
+                    Container(
+                      color: Colors.red,
+                    ),
+                    Container(
+                      color: Colors.green,
+                    ),
+                    Container(
+                      color: Colors.yellow,
+                    ),
+                  ],
+                ),
+                Container(
+                  alignment: Alignment(0, 0.9),
+                  child: SmoothPageIndicator(
+                        controller: Controler.pageController,
+                        count: 3,
+                        effect: const WormEffect(
+                            dotWidth: 13,
+                            dotHeight: 13,
+                            dotColor: fotterTextColor,
+                            activeDotColor: primaryColor),
+                      ),
+                  )
+                ]
               )
             ),
           ),
+
+
           SliverToBoxAdapter(
               child: Column(
                 children: [
